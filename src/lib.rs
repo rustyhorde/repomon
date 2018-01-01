8dfd82df59249dade8f381bdd82f5c7f3cf421ee
Jason Ozias <jason.g.ozias@gmail.com>
2018-01-01 12:49:23 -0500

    Updated headers

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 repomon-config developers
+// Copyright (c) 2017 repomon developers
 //
 // Licensed under the Apache License, Version 2.0
 // <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0> or the MIT
